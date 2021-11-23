169d6ce62481bac3f4d0db6b43cc63f63d40c218
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2021-11-23 23:33:03 +0100

    add constraint decorator

--- src/domain/decorators/timezone.decorator.ts
+++ src/domain/decorators/timezone.decorator.ts
@@ -4,15 +4,17 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 export const Timezone = createParamDecorator(
     (data: unknown, context: ExecutionContext) =>
     {
+        let request;
+
         if (context['contextType'] === 'graphql')
         {
-            const request = GqlExecutionContext.create(context).getContext().req;
-            return request.header('X-Timezone') ? request.header('X-Timezone') : process.env.TZ;
+            request = GqlExecutionContext.create(context).getContext().req;
         }
         else if (context['contextType'] === 'http')
         {
-            const request = context.switchToHttp().getRequest();
-            return request.header('X-Timezone') ? request.header('X-Timezone') : process.env.TZ;
+            request = context.switchToHttp().getRequest();
         }
+
+        return request.header('X-Timezone') ? request.header('X-Timezone') : process.env.TZ;
     }
 );
