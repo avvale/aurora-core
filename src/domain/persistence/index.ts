2e3ee2611a229e0a310033708e671d55137cb988
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2021-11-29 18:48:29 +0100

    wip

--- src/domain/persistence/index.ts
+++ src/domain/persistence/index.ts
@@ -1,3 +1,5 @@
 export * from './criteria';
 export * from './repository';
+export * from './sql-statement/index-hints';
+export * from './sql-statement/operator';
 export * from './sql-statement/sql-statement';
