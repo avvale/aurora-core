078a7d3c4e9f818b7d2e94f18b7d2c6e94a2cce1
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2022-04-27 23:06:35 +0200

    add seeds

---
+++ src/domain/persistence/seeds/iam-account.seed.ts
@@ -0,0 +1,18 @@
+import { users } from '.';
+
+export const accounts = [
+    {
+        id               : '948a5308-a49d-42dc-9ea3-7490e120000b',
+        type             : 'USER',
+        email            : 'john.doe@gmail.com',
+        isActive         : true,
+        clientId         : '333910d9-394b-42d7-b3e0-0c7ae7a54478',
+        dApplicationCodes: ['quasar'],
+        dPermissions     : {},
+        dTenants         : [],
+        data             : null,
+        roleIds          : [],
+        tenantIds        : [],
+        user             : users[0],
+    },
+];
