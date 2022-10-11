5778ba537d2044f04a01c275d97f3552bd48977a
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2022-10-11 11:30:11 +0200

    add blob value object

--- src/domain/value-objects/blob.value-object.ts
+++ src/domain/value-objects/blob.value-object.ts
@@ -1,9 +1,17 @@
-import { StringValueObject } from './string.value-object';
+import { BadRequestException } from '@nestjs/common';
+import { ValueObject } from './value-object';
 
-export abstract class BlobValueObject extends StringValueObject
+export abstract class BlobValueObject extends ValueObject<string | Buffer>
 {
+    set value(value: string | Buffer)
+    {
+        if (value instanceof Buffer) value = value.toString('base64');
+        super.value = value;
+    }
+
     get buffer(): Buffer
     {
-        return Buffer.from(super.value, 'base64');
+        if (typeof super.value === 'string') return Buffer.from(super.value, 'base64');
+        throw new BadRequestException(`The value for BlobValueObject must must to be a string base64, your type is ${typeof super.value}`);
     }
 }
