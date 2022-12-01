540efd457f28143e27281bc4ed6edd5624d13f12
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2022-12-01 19:27:13 +0100

    UuidArrayValueObject inherits  from JsonValueObject & upgrade version

--- src/domain/value-objects/uuid-array.value-object.ts
+++ src/domain/value-objects/uuid-array.value-object.ts
@@ -1,19 +1,14 @@
-import { ValueObject } from './value-object';
+import { JsonValueObject } from './json.value-object';
 
-export abstract class UuidArrayValueObject extends ValueObject<string[]>
+export abstract class UuidArrayValueObject extends JsonValueObject
 {
     get length(): number
     {
-        return super.value?.length;
+        return this.value?.length;
     }
 
     isArray(): boolean
     {
         return Array.isArray(super.value);
-    }
-
-    toString(): string
-    {
-        return super.value.toString();
     }
 }
