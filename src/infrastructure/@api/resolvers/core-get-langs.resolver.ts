b7a05e8f14702fe98918275bde2d9b84c2ac5b74
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-06-07 10:46:18 +0200

    minor fix

--- src/infrastructure/@api/resolvers/core-get-langs.resolver.ts
+++ src/infrastructure/@api/resolvers/core-get-langs.resolver.ts
@@ -1,15 +1,16 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { Resolver, Query } from '@nestjs/graphql';
 import { CoreGetLangsHandler } from '../handlers';
 
 @Resolver()
-export class CommonGetCountriesResolver
+export class CoreGetLangsResolver
 {
     constructor(
         private readonly handler: CoreGetLangsHandler,
     ) {}
 
     @Query('coreGetLangs')
-    async main(): Promise<any[]>
+    async main()
     {
         return await this.handler.main();
     }
