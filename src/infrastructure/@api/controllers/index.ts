8fa3329aaccf2206dbcc32855be478ce861b6485
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-06-08 10:53:38 +0200

    wip

--- src/infrastructure/@api/controllers/index.ts
+++ src/infrastructure/@api/controllers/index.ts
@@ -1,4 +1,5 @@
 export { CoreEnvironmentInformationController } from './core-environment-information.controller';
+export { CoreGetFallbackLangController } from './core-get-fallback-lang.controller';
 export { CoreGetLangsController } from './core-get-langs.controller';
 export { CorePreparationRequestController } from './core-preparation-request.controller';
 export { CoreStatusController } from './core-status.controller';
