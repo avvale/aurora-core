1680ceb467c48e23cafb10e7a3e36dec6856923b
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-07-12 12:41:57 +0200

    fix nullable and undefinable BlobValueObject

--- src/domain/value-objects/blob.value-object.ts
+++ src/domain/value-objects/blob.value-object.ts
@@ -11,13 +11,19 @@ export abstract class BlobValueObject extends ValueObject<string | Buffer>
 
     get value(): string
     {
+        if (this.validationRules.nullable && super.value === null) return null;
+        if (this.validationRules.undefinable && super.value === undefined) return undefined;
+
         if (typeof super.value === 'string') return super.value;
         throw new BadRequestException(`The value for BlobValueObject must must to be a string base64, your type is ${typeof super.value}`);
     }
 
     get buffer(): Buffer
     {
+        if (this.validationRules.nullable && this.value === null) return null;
+        if (this.validationRules.undefinable && this.value === undefined) return undefined;
+
         if (typeof super.value === 'string') return Buffer.from(super.value, 'base64');
-        throw new BadRequestException(`The value for BlobValueObject must must to be a string base64, your type is ${typeof super.value}`);
+        throw new BadRequestException(`The value for BlobValueObject to get buffer must must to be a string base64, your type is ${typeof super.value}`);
     }
 }
