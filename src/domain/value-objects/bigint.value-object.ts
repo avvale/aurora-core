0c776ab65ecbc04292653771914f143ebc6afce0
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2025-11-01 19:59:49 +0100

    feat: update BigintValueObject to extend NumberValueObject and enhance value setter logic

--- src/domain/value-objects/bigint.value-object.ts
+++ src/domain/value-objects/bigint.value-object.ts
@@ -1,4 +1,16 @@
-import { IntValueObject } from './int.value-object';
+import { NumberValueObject } from './number.value-object';
 
-export abstract class BigintValueObject extends IntValueObject
-{}
+export abstract class BigintValueObject extends NumberValueObject
+{
+    set value(value: number | null | undefined)
+    {
+        if (typeof value === 'string')
+        {
+            super.value = value ? Number(value) : null;
+        }
+        else
+        {
+            super.value = value;
+        }
+    }
+}
