314c83ab4b2bbe8bc9a4b99c7e6b109e1fa36ec6
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2026-01-18 23:59:59 +0100

    refactor: simplify getPackageFile function and improve EnumValueObject type safety

--- src/domain/functions/get-package-file.function.ts
+++ src/domain/functions/get-package-file.function.ts
@@ -1,7 +1,6 @@
 import { LiteralObject } from '@domain/types';
 import * as fs from 'node:fs';
 
-export const getPackageFile = (): LiteralObject =>
-{
-    return JSON.parse(fs.readFileSync(process.cwd() + '/package.json', 'utf8'));;
+export const getPackageFile = (): LiteralObject => {
+    return JSON.parse(fs.readFileSync(process.cwd() + '/package.json', 'utf8'));
 };
--- src/domain/value-objects/enum.value-object.ts
+++ src/domain/value-objects/enum.value-object.ts
@@ -1,20 +1,19 @@
 import { BadRequestException } from '@nestjs/common';
 import { StringValueObject } from './string.value-object';
 
-export abstract class EnumValueObject extends StringValueObject
-{
-    set value(value: string)
-    {
-        if (
-            value &&
-            !this.validationRules.enumOptions.includes(value)
-        ) throw new BadRequestException(`Value for ${this.validationRules.name} has to be any of this options: ${this.validationRules.enumOptions.join(', ')}`);
+export abstract class EnumValueObject<
+    T extends string = string,
+> extends StringValueObject {
+    set value(value: T) {
+        if (value && !this.validationRules.enumOptions.includes(value))
+            throw new BadRequestException(
+                `Value for ${this.validationRules.name} has to be any of this options: ${this.validationRules.enumOptions.join(', ')}`,
+            );
 
         super.value = value;
     }
 
-    get value(): string
-    {
-        return super.value;
+    get value(): T {
+        return super.value as T;
     }
 }
